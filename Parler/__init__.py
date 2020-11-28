f9f2d2dbff353156afc9412a8365501b4b9d31a3
ExtraLime <willdox7@live.com>
2020-11-28 11:40:49 -0800

    added response handler,reconnect limit for api call methods. Modified the methods to call themselves recursively if select error responses are received.

--- Parler/__init__.py
+++ Parler/__init__.py
@@ -3,163 +3,179 @@ from typing import List
 import random
 import string
 import json
+from time import sleep
 
 class Parler:
 
 
-	@staticmethod
-	def get_login_key(email, password):
-		data = {
-			"identifier": email,
-			"password": password,
-			"deviceId": "".join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(16))
-		}
-		response = requests.post('https://api.parler.com/v2/login/new', data=json.dumps(data))
-		return response.json()
-	
-	@staticmethod
-	def get_chapta_image(key):
-		data = {
-			"identifier": key
-		}
+    @staticmethod
+    def get_login_key(email, password):
+        data = {
+            "identifier": email,
+            "password": password,
+            "deviceId": "".join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(16))
+        }
+        response = requests.post('https://api.parler.com/v2/login/new', data=json.dumps(data))
+        return response.json()
+    
+    @staticmethod
+    def get_chapta_image(key):
+        data = {
+            "identifier": key
+        }
 
-		response = requests.post('https://api.parler.com/v2/login/captcha/new', data=json.dumps(data))
-		return response.json()
+        response = requests.post('https://api.parler.com/v2/login/captcha/new', data=json.dumps(data))
+        return response.json()
 
-	@staticmethod
-	def submit_chapta(key, solution):
-		data ={
-			"identifier": key,
-			"solution":solution
-		}
+    @staticmethod
+    def submit_chapta(key, solution):
+        data ={
+            "identifier": key,
+            "solution":solution
+        }
 
-		response = requests.post('https://api.parler.com/v2/login/captcha/submit',data=json.dumps(data))
-		return response.json()
-	"""
-	:param jst: short-term token
-	:param mst: master token
-	:param debug: print debugging messages
-	"""
-	def __init__(self, jst: str, mst: str, debug: bool):
-		self.jst = jst
-		self.mst = mst
-		self.debug = debug
-		self.base_url = "https://api.parler.com/v1"
-		self.session = requests.Session()
+        response = requests.post('https://api.parler.com/v2/login/captcha/submit',data=json.dumps(data))
+        return response.json()
+    """
+    :param jst: short-term token
+    :param mst: master token
+    :param debug: print debugging messages
+    """
+    def __init__(self, jst: str, mst: str, debug: bool):
+        self.jst = jst
+        self.mst = mst
+        self.debug = debug
+        self.base_url = "https://api.parler.com/v1"
+        self.session = requests.Session()
 
-		self.session.cookies.set("mst", mst)
-		self.session.cookies.set("jst", jst)
+        self.session.cookies.set("mst", mst)
+        self.session.cookies.set("jst", jst)
+        self.reconnects = 0
 
-	"""
-	:param username: Username to fetch
-	"""
-	def profile(self, username=None) -> dict:
-		response = self.session.get(self.base_url + "/profile")
-		if username is not None:
-			params = (
-				("username", username),
-			)
-			response = self.session.get(self.base_url + "/profile", params=params)
-		if response.status_code != 200:
-			raise Exception({"status": response.status_code})
-		return response.json()
-	
-	"""
-	:param searchtag: Hashtag to search
-	"""
-	def hashtags(self, searchtag="") -> dict:
-		params = (
-			("search", searchtag),
-		)
-		response = self.session.get(self.base_url + "/hashtag",  params=params)
-		if response.status_code != 200:
-			raise Exception({"status": response.status_code})
-		return response.json()
+        
+    """
+    @helper response handler
+    pass an http response through to check for specific codes
+    """
+    
+    def handle_response(self,response):
+        if self.reconnects >= 10:
+            raise Exception ("Internal abort; 10 reconnect attemps")
+        elif response.status_code >=400 and response.status_code <=428:
+            raise Exception ({'status':response.status_code,
+                              'error':response.reason,
+                             'English': "Most likely unauthorized or no results"})
+            
+        elif response.status_code == 502:
+            print('Bad Gateway Error, retry in 5 seconds')
+            self.reconnects += 1
 
-	"""
-	:param limit: limit
-	:param cursor: string to id the next items
-	"""
-	def feed(self, limit=10, cursor="") -> dict:
-		params = (
-			("limit", limit),
-		)
-		if cursor != "":
-			params = params + (("startkey",cursor),)
-		response = self.session.get(self.base_url + "/feed",  params=params)
-		if response.status_code != 200:
-			raise Exception({"status": response.status_code})
-		return response.json()
-		
-	"""
-	:param limit: limit
-	:param cursor: string to id the next items
-	"""
-	def notifications(self, limit=10, cursor="") -> dict:
-		params = (
-			("limit", limit),
-		)
-		if cursor != "":
-			params = params + (("startkey",cursor),)
-		response = self.session.get(self.base_url + "/notification",  params=params)
-		if response.status_code != 200:
-			raise Exception({"status": response.status_code})
-		return response.json()
+        elif response.status_code == 429:
+            print('Too many requests Error, retry in 5 seconds')
+            self.reconnects += 1
 
-	"""
-	:param limit: limit
-	:param cursor: string to id the next items
-	"""
-	def discover_feed(self, limit=10, cursor="") -> dict:
-		params = (
-			("limit", limit),
-		)
-		if cursor != "":
-			params = params + (("startkey",cursor),)
-		response = self.session.get(self.base_url + "/discover/posts",  params=params)
-		if response.status_code != 200:
-			raise Exception({"status": response.status_code})
-		return response.json()
+        else:
+            self.reconnects = 0
+        
+        return response
+        
+    """
+    :param username: Username to fetch
+    """
+    def profile(self, username=None) -> dict:
+        response = self.session.get(self.base_url + "/profile")
+        if username is not None:
+            params = (
+                ("username", username),
+            )
+            response = self.session.get(self.base_url + "/profile", params=params)
+        if self.handle_response(response).status_code != 200:
+            print(f'Status: {response.status_code}')
+            sleep(5)
+            return self.profile(username)
+        
+        return response.json()
 
-	"""
-	:param tag: hashtag to get feed from
-	:param limit: limit
-	:param cursor: string to id the next items
-	"""
-	def hashtags_feed(self, tag, limit=10, cursor="") -> dict:
-		params = (
-			("tag", tag),
-			("limit", limit)
-		)
-		if cursor != "":
-			params = params + (("startkey",cursor),)
-		response = self.session.get(self.base_url + "/post/hashtag",  params=params)
-		if response.status_code != 200:
-			raise Exception({"status": response.status_code})
-		return response.json()
+    """
+    :param searchtag: Hashtag to search
+    """
+    def hashtags(self, searchtag="") -> dict:
+        params = (
+            ("search", searchtag),
+        )
+        response = self.session.get(self.base_url + "/hashtag",  params=params)
+        if self.handle_response(response).status_code != 200:
+            print(f'Status: {response.status_code}')
+            sleep(5)
+            return self.hashtags(searchtag)
+        return response.json()
 
-	"""
-	:param search: search term
-	"""
-	def users(self, searchtag="") -> dict:
-		params = (
-			("search", searchtag),
-		)
-		response = self.session.get(self.base_url + "/users", params=params)
-		if response.status_code != 200:
-			raise Exception({"status": response.status_code})
-		return response.json()
+    """
+    :param limit: limit
+    :param cursor: string to id the next items
+    """
+    def feed(self, limit=10, cursor="") -> dict:
+        params = (
+            ("limit", limit),
+        )
+        if cursor != "":
+            params = params + (("startkey",cursor),)
+        response = self.session.get(self.base_url + "/feed",  params=params)
+        if self.handle_response(response).status_code != 200:
+            print(f'Status: {response.status_code}')
+            sleep(5)
+            return self.feed(limit,cursor)
+        return response.json()
+        
+    """
+    :param limit: limit
+    :param cursor: string to id the next items
+    """
+    def notifications(self, limit=10, cursor="") -> dict:
+        params = (
+            ("limit", limit),
+        )
+        if cursor != "":
+            params = params + (("startkey",cursor),)
+        response = self.session.get(self.base_url + "/notification",  params=params)
+        if self.handle_response(response).status_code != 200:
+            print(f'Status: {response.status_code}')
+            sleep(5)
+            return self.notifications(limit, cursor)
+        return response.json()
 
-	def follow_user(self, username) -> dict:
-		params = (
-			("username", username),
-		)
-		data = json.dumps({"username": username})
-		headers = {
-			'Accept': 'application/json, text/plain, */*',
-			'Content-Type': 'application/json',
-		}
-		response = self.session.post(self.base_url + "/follow", params=params, data=data, headers=headers)
-		if response.status_code != 200:
-			raise Exception({"status": response.status_code})
-		return response.json()
+    """
+    :param limit: limit
+    :param cursor: string to id the next items
+    """
+    def discover_feed(self, limit=10, cursor="") -> dict:
+        params = (
+            ("limit", limit),
+        )
+        if cursor != "":
+            params = params + (("startkey",cursor),)
+        response = self.session.get(self.base_url + "/discover/posts",  params=params)
+        if self.handle_response(response).status_code != 200:
+            print(f'Status: {response.status_code}')
+            sleep(5)
+            return self.discover_feed(limit, cursor)
+        return response.json()
+
+    """
+    :param tag: hashtag to get feed from
+    :param limit: limit
+    :param cursor: string to id the next items
+    """
+    def hashtags_feed(self, tag, limit=10, cursor="") -> dict:
+        params = (
+            ("tag", tag),
+            ("limit", limit)
+        )
+        if cursor != "":
+            params = params + (("startkey",cursor),)
+        response = self.session.get(self.base_url + "/post/hashtag",  params=params)
+        if self.handle_response(response).status_code != 200:
+            print(f'Status: {response.status_code}')
+            sleep(5)
+            return self.hashtags_feed(limit, cursor)
+        return response.json()
