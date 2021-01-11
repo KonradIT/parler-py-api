86d15bff2f2413e08126a3cf1cec9a19e1b68adc
KonradIT <github@chernowii.com>
2021-01-11 13:13:02 +0100

    Get comments, update README

--- README.md
+++ README.md
@@ -4,14 +4,20 @@
 
 This **UNOFFICIAL** library designed to programatically fetch data from parler.com
 
+# IMPORTANT ANNOUNCEMENT:
+
+Parler.com is down, [they got dumped by their cloud host AWS](https://www.buzzfeednews.com/article/johnpaczkowski/amazon-parler-aws) and should they go online again I will update this project accordingly.
+
+⇙⇙⇙
+
 ## Authentication:
 
 You need to create a `.parler.env` file with the following contents:
 
-````
+```
 JST=<your browser JST>
 MST=<your browser MST>
-````
+```
 
 Get them from Inspect element > storage
 
@@ -21,14 +27,14 @@ Get them from Inspect element > storage
 
 Can show hashtags, feed
 
-````
+```
 usage: parlerctl.py [-h] --show SHOW [--summary]
 parlerctl.py: error: the following arguments are required: --show
-````
+```
 
-- profile
-- hashtags
-- ingest
+-   profile
+-   hashtags
+-   ingest
 
 ## Installation
 
@@ -55,7 +61,6 @@ Other experiments available as well.
 
 Feel free to improve the code, submit your experiments and sample code or fix bugs. Before submitting a PR, run `pep8` linter on your code.
 
-
 ## Donate:
 
 If you found this package useful, [consider donating](https://paypal.me/konradit).
@@ -66,6 +71,6 @@ I am in now way affiliated with Parler, it's subsidiaries or any entity related 
 
 2009/24/EC Art 5:
 
-````
+```
 The person having a right to use a copy of a computer program shall be entitled, without the authorisation of the rightholder, to observe, study or test the functioning of the program in order to determine the ideas and principles which underlie any element of the program if he does so while performing any of the acts of loading, displaying, running, transmitting or storing the program which he is entitled to do.
-````
+```
--- experiments/03_posts_from_user.py
+++ experiments/03_posts_from_user.py
@@ -21,9 +21,32 @@ load_dotenv(dotenv_path=".parler.env")
 parler = Parler(jst=os.getenv("JST"), mst=os.getenv("MST"), debug=True)
 
 filename = "%s/data/usernames/%s_%s_%s.csv"
-max_sleep_limit = 5
+max_sleep_limit = 6
 
 logging.basicConfig(level=logging.DEBUG)
+
+def get_comments(username, thread_id, output_dir):
+    data = parler.comments(thread_id, cursor="")
+    feed = models.FeedSchema().load(data)
+    while True:
+        if feed.last:
+            logging.info("Exiting, all done.")
+            break
+        try:
+            data = parler.comments(thread_id, cursor=feed.next)
+            feed = models.FeedSchema().load(data)
+        except Exception as e:
+            if e.__class__.__name__ == "TypeError:":
+                sys.exit()
+            time.sleep(max_sleep_limit)
+        finally:
+            logging.info("Writing to file...")
+            with open(filename % (output_dir, username + "_" + thread_id, str(datetime.date.today()), "users"), mode="a", encoding="utf-8") as users:
+                exputils.writetocsv(users, feed.users, insert_headers=False)
+            with open(filename % (output_dir, username + "_" + thread_id, str(datetime.date.today()), "comments"), mode="a", encoding="utf-8") as links:
+                exputils.writetocsv(links, feed.links, insert_headers=False)
+        time.sleep(random.randint(max_sleep_limit/2, max_sleep_limit))
+
 def get_posts(username, output_dir):
     logging.info("Starting collection of posts from user %s" % username)
     
@@ -42,6 +65,8 @@ def get_posts(username, output_dir):
         exputils.writetocsv(posts, feed.items, insert_headers=True)
     with open(filename % (output_dir, username, str(datetime.date.today()), "links"), mode="a", encoding="utf-8") as links:
         exputils.writetocsv(links, feed.links, insert_headers=True)
+    for thread in feed.items:
+                get_comments(username=username, thread_id=thread.get("Id"), output_dir=output_dir)
     while True:
         logging.info("is last? %s", feed.last)
         if feed.last:
@@ -63,7 +88,8 @@ def get_posts(username, output_dir):
                 exputils.writetocsv(posts, feed.items)
             with open(filename % (output_dir, username, str(datetime.date.today()), "links"), mode="a", encoding="utf-8") as links:
                 exputils.writetocsv(links, feed.links)
-
+            for thread in feed.items:
+                get_comments(username=username, thread_id=thread.get("Id"), output_dir=output_dir)
 
 if __name__ == "__main__":
     
